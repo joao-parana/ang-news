fd0a6533f4011ff6fc9f6558c2a3296544b7c1d5
joao-parana <joao.parana@gmail.com>
2014-05-25 12:01:37 -0300

    Versão com Bootstrap, ngInclude e filtros melhorando a UI

---
+++ app/scripts/controllers/nav.js
@@ -0,0 +1,16 @@
+'use strict';
+ 
+app.controller('NavCtrl', function ($scope, $location, Post) {
+    $scope.post = {url: 'http://', title: ''};
+ 
+    $scope.submitPost = function () {
+      Post.create($scope.post).then(function (ref) {
+        // $location.path('/posts/' + ref.name());
+        console.log(ref.name());
+        $location.path('/');
+	      $scope.post = { url: 'http://', title: '' };
+      });
+    };
+ 
+  });
+
--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -32,13 +32,13 @@ app.controller('PostsCtrl', function ($scope, $location, Post) {
   // $scope.post = {url: 'http://', 'title': ''};
   $scope.post = { url: 'http://' };
  
-  $scope.submitPost = function () {
-    // Post.create($scope.post).then(function () {
-    Post.create($scope.post).then(function (ref) {
-      // $scope.post = {url: 'http://', 'title': ''};
-      $location.path('/posts/' + ref.name());
-    });
-  };
+  // $scope.submitPost = function () {
+  //  // Post.create($scope.post).then(function () {
+  //  Post.create($scope.post).then(function (ref) {
+  //    // $scope.post = {url: 'http://', 'title': ''};
+  //    $location.path('/posts/' + ref.name());
+  //  });
+  // };
  
   $scope.deletePost = function (postId) {
     Post.delete(postId);
