2ad963a8f9145a3f96628e8339eb23bbd79afbf5
João Paraná <joao.parana@gmail.com>
2014-06-04 12:42:16 -0300

    adicionando CRUD de projetos

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* global app:true */
- 
+
 angular
   .module('angNewsApp', [
     'ngCookies',
@@ -27,6 +27,18 @@ angular
         templateUrl: 'views/login.html',
         controller: 'AuthCtrl'
       })
+      .when('/projects', {
+        controller:'ProjectListCtrl',
+        templateUrl:'views/projectlist.html'
+      })
+      .when('/project/edit/:projectId', {
+        controller:'ProjectEditCtrl',
+        templateUrl:'views/projectdetail.html'
+      })
+      .when('/project/new', {
+        controller:'ProjectCreateCtrl',
+        templateUrl:'projectdetail.html'
+      })
       .otherwise({
         redirectTo: '/'
       });
@@ -35,4 +47,44 @@ angular
 var app = angular.module('angNewsApp');
 app.constant('FIREBASE_URL',
     'https://luminous-fire-6652.firebaseio.com/');
+
+app.factory('Projects', function($firebase, fbURL) {
+  return $firebase(new Firebase(fbURL));
+})
+
+.config(function($routeProvider) {
+  $routeProvider
+
+    .otherwise({
+      redirectTo:'/'
+    });
+})
+
+.controller('ProjectListCtrl', function($scope, Projects) {
+  $scope.projects = Projects;
+})
+
+.controller('CreateCtrl', function($scope, $location, $timeout, Projects) {
+  $scope.save = function() {
+    Projects.$add($scope.project, function() {
+      $timeout(function() { $location.path('/'); });
+    });
+  };
+})
+
+.controller('EditCtrl',
+  function($scope, $location, $routeParams, $firebase, fbURL) {
+    var projectUrl = fbURL + $routeParams.projectId;
+    $scope.project = $firebase(new Firebase(projectUrl));
+
+    $scope.destroy = function() {
+      $scope.project.$remove();
+      $location.path('/');
+    };
+
+    $scope.save = function() {
+      $scope.project.$save();
+      $location.path('/');
+    };
+});
 
